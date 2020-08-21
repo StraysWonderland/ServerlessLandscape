479f962899de3ae38da0cd9249ba9049b6c009b4
Cagri Tasci <cagri.c.tasci@outlook.de>
2020-08-21 17:18:20 +0200

    Update LocustFaasTest.py

--- LoadTesting/LocustFaasTest.py
+++ LoadTesting/LocustFaasTest.py
@@ -1,13 +1,21 @@
 import random
+import json
 from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
     wait_time = between(5, 9)
 
-    @task
-    def index_page(self):
-        self.client.post("/function/markdown", {"# locust load test *client posting to markdown function"})
+ #   @task
+ #   def index_page(self):
+ #       self.client.get("/function/break-even-kotlin")
         
-
-    def on_start(self):
-       self.client.get("/function/break-even-kotlin")
+    @task    
+    def create_post(self):
+        headers = {'content-type': 'application/json','Accept-Encoding':'gzip'}
+        self.client.post("/function/break-even-kotlin",data= json.dumps({
+      "price": 10.0,
+      "fixedCosts": 100.0,
+      "unitCosts": 20.0
+    }), 
+    headers=headers, 
+    name = "break-even-kotlin")
