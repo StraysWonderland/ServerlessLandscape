6a5b8c0d493b0331db43c2fb88b99573864d74af
Cagri Tasci <cagri.c.tasci@outlook.de>
2020-08-21 17:18:13 +0200

    Create LocustMinikubeTest.py

---
+++ LoadTesting/LocustMinikubeTest.py
@@ -0,0 +1,13 @@
+import random
+from locust import HttpUser, task, between
+
+class QuickstartUser(HttpUser):
+    wait_time = between(5, 9)
+
+    @task
+    def index_page(self):
+        self.client.post("/run?price=20.00&fixedCost=100.00&unitCost=10.00")
+        
+
+    def on_start(self):
+       self.client.get("/run?price=20.00&fixedCost=100.00&unitCost=10.00")
